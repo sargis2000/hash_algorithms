430182f89175ecd4cd788c23a10ec1289ff4f8a5
sargis <khachatryansargis2000@gmail.com>
2022-09-19 16:58:51 +0000

    somethigin settings

--- hash_algorithms/settings.py
+++ hash_algorithms/settings.py
@@ -21,11 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if str(os.getenv('DEPLOY')) == 'True':
+    DEBUG = False
+    ALLOWED_HOSTS = ['hashalgorithms.pythonanywhere.com']
+else:
+    DEBUG = True
+    ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = ['']
 
 
 # Application definition
